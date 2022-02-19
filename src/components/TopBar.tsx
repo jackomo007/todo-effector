ec546954c7171dc06bbf765329d80afe6ec3668c
Angel <jeal.code47@gmail.com>
2022-02-19 15:55:52 -0300

    feat: load asynchronous

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import { Button, Grid } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import {getTodos} from "../store"
 
 function TopBar() {
   return (
     <Grid pt={2} templateColumns="1fr 1fr" columnGap="3">
       <ColorModeSwitcher />
-      <Button>Load</Button>
+      <Button onClick={() => getTodos('https://raw.githubusercontent.com/jackomo007/todo-effector/master/src/mock.json')}>Load</Button>
     </Grid>
   );
 }
